7fd8c2b95348c8a21e6845ed2aa57d799245774e
Parth Sharma <parthsharma8421@gmail.com>
2023-01-29 14:18:16 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,9 +9,9 @@ export const Footer = ({ className }) => (
       <span className={styles.date}>
         {`© ${new Date().getFullYear()} Parth Sharma.`}
       </span>
-      <Link secondary className={styles.link} href="/humans.txt" target="_self">
-        Crafted by yours truly
-      </Link>
+//       <Link secondary className={styles.link} href="/humans.txt" target="_self">
+//         Crafted by yours truly
+//       </Link>
     </Text>
   </footer>
 );
